d733ea6f7d4e787ac62fc838f871622e684ac96f
mcmanus-git <melancholic_maclean@yahoo.com>
2021-07-31 22:15:50 -0400

    Added comments on available themes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
 
+# Toggle the themes at [dbc.themes.LUX]
+# The full list of available themes is:
+# CERULEAN, COSMO, CYBORG, DARKLY, FLATLY, JOURNAL, LITERA, LUMEN, LUX, MATERIA, MINTY, PULSE, SANDSTONE,
+# SIMPLEX, SKETCHY, SLATE, SOLAR, SPACELAB, SUPERHERO, UNITED, YETI.
+# https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
+
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
 server = app.server
